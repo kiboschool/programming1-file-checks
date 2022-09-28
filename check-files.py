f0e29829e3c2514530172f226126039706744879
Rob Cobb <rwcobbjr@gmail.com>
2022-09-27 22:20:11 -0600

    renames

--- check-files.py
+++ check-files.py
@@ -1,8 +1,18 @@
 import os
 
+
 def check_files():
-    # Implement this function
-    pass
+    filename = input("Enter a file name: ")
+    if os.path.exists(filename):
+        with open(filename, 'r') as lines_file:
+            lines = lines_file.read().splitlines()
+        for line in lines:
+            if os.path.exists(line):
+                print(f"File {[line]} exists!")
+            else:
+                print(f"File {[line]} does not exist!")
+    else:
+        print(f"Usage: Filename [{filename}] does not exist")
 
 
 if __name__ == "__main__":
